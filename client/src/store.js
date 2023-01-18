8223984faa23821e1d88d8eababf971415c38e29
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-18 23:38:23 +0200

    updates with new reducers to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userReducer from "./features/User/userSlice"
 import courseReducer from './features/Courses/courseSlice'
+import lessonReducer from './features/Lessons/lessonSlice'
+import videoReducer from './features/Videos/videoSlice'
 
 const store = configureStore({
     reducer: {
         user: userReducer,
-        course: courseReducer
+        courses: courseReducer, 
+        lesson: lessonReducer,
+        videos: videoReducer,
     } 
 })
 
