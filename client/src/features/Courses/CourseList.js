0d64856dd377d2b2e13871fbbd403a1bfdd52669
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-18 23:37:24 +0200

    create course list for home page

---
+++ client/src/features/Courses/CourseList.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+
+import {useSelector, useDispatch} from "react-redux"
+
+import { setCourses } from './courseSlice'
+
+import { Box, Card, CardContent, Button, Typography, Grid } from '@mui/material'
+
+import CourseCard from './CourseCard'
+
+function CourseList() {
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(true)
+    const courses = useSelector(state=> state.courses.courses)
+    console.log(courses)
+
+    
+
+    useEffect(()=>{
+        fetch('/courses')
+        .then((r)=>r.json())
+        .then((courses)=>{
+            dispatch(setCourses(courses))
+            setLoading(false)
+        })
+    }, [])
+
+    let coursesToDisplay
+  
+     if(loading === false){
+        coursesToDisplay = courses.map((course)=>{
+        return(
+            <Box sx={{justifyContent: 'center'}}>
+                <CourseCard course = {course}></CourseCard>
+            </Box>
+        )
+    })
+    }
+
+    return (
+        
+        <div>
+            {loading ?
+            <div>loading...</div>
+            :
+            <div>
+            <Box sx = {{display: 'grid', gridTemplateColumns: 'repeat(4, 2fr)', gap: 2, m: 3, flexWrap: 'wrap' }} container spacing={3}>
+                {coursesToDisplay}
+            </Box>
+            </div>
+        }
+        </div>
+    )
+}
+
+export default CourseList
