3ae6ec7def3df3e932ff8b750396a04058ba6d66
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-18 23:40:17 +0200

    adds working link_valid? method to Video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,14 +1,27 @@
 require 'net/http'
+
 class Video < ApplicationRecord
     belongs_to :lesson
 
 
     def self.link_valid?(link)
-        url = URI.parse(link)
-        req = Net::HTTP::Get.new url
-        ()
-        res = Net::HTTP.start(url.host, url.port, 
-            :use_ssl => url.scheme=='https'){|http| http.request req}
-        puts res.
+        key = ENV["YOUTUBE_API_KEY"]
+        url = Yt::URL.new link
+        id = url.id
+
+        uri = URI('https://www.googleapis.com/youtube/v3/videos')
+        params = {:key => key, :part => 'snippet', :id=>id}
+        uri.query = URI.encode_www_form(params)
+        response = Net::HTTP.get_response(uri)
+
+        parsed_response = JSON.parse(response.body)
+
+         if parsed_response["items"].length == 0
+            puts "invalid"
+            false
+         else
+            puts "valid"
+            true
+         end
     end
 end
