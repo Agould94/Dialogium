b11d840c5270ec4eabb97da0d5133b16e33f5ce8
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-18 23:37:04 +0200

    update to course page views

--- client/src/features/Courses/CoursePage.js
+++ client/src/features/Courses/CoursePage.js
@@ -1,44 +1,86 @@
-import React, { useEffect } from "react";
-import {useParams} from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import {useParams, Link} from "react-router-dom"
+
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+
+import Button from '@mui/material/Button';
+
 import {setCourse} from "./courseSlice"
+
+import { setLesson } from "../Lessons/lessonSlice";
+
+import { resetVideos } from "../Videos/videoSlice";
 
 function CoursePage(){
     let params = useParams()
     const dispatch = useDispatch()
-    const course = useSelector(state=> state.course.course)
-    console.log(course)
-    console.log(params)
+    console.log("hello")
 
+    const [loading, setLoading] = useState(true)
+    console.log(loading)
+    
+    console.log("below")
+    //console.log(course)
+   
+    console.log(params)
     useEffect(()=>{
+        if(Object.keys(course).length === 0){
         fetch(`/courses/${params.id}`)
         .then((r)=>r.json())
         .then((course)=>{
             dispatch(setCourse(course))
+            setLoading(false)
+            dispatch(resetVideos())
+            //course = useSelector(state=> state.courses.course)
         })
+        }else{
+            setLoading(false)
+            dispatch(resetVideos())
+        }
     }, [])
 
-    const displaySections = course.sections.map((section, index)=>{
-        const lessons = section.lessons.map((lesson)=>{
+    const course = useSelector(state=> state.courses.course)
+    console.log(course)
+
+    function clearLesson(){
+        dispatch(setLesson({}))
+    }
+
+    let displaySections
+    if(loading === false){
+        displaySections = course.sections.map((section, index)=>{
+        const lessons = section.lessons.map((lesson, lessonIndex)=>{
             return(
-            <li key = {lesson.id}>{lesson.name}</li>
+            <li>
+                <Link to = {`/courses/${course.id}/sections/${index}/lessons/${lessonIndex}`} key = {lesson.id}>{lesson.name}</Link>
+            </li>
             )
         })
         return(
             <div key = {index}>
-                <h2>Section {index+1}</h2>
+                <h2>Section {index+1}:</h2>
                 <h3>Lessons:</h3>
                 <ol>
                     {lessons}
                 </ol>
+                <Link to = {`/courses/${course.id}/sections/${index}/new`}>
+                    <Button onClick = {clearLesson} variant = "contained">Add Lesson</Button>
+                </Link>
             </div>
         )
     })
+}
     return(
         <div>
-            <h1>{course.title}</h1>
-            <div>{displaySections}</div>
+            {loading ?
+            <div>loading</div>
+            :
+            <div>
+                <h1>{course.title.slice(6, course.title.length)}</h1>
+                <div>{displaySections}</div>
+            </div>
+            }   
         </div>
     )
 }
