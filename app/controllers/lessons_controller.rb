5a3a0ed8500884f2154a554dcf0ee81853997372
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-22 23:16:29 +0200

    added back end validations to create course

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -11,18 +11,22 @@ class LessonsController < ApplicationController
 
     def create
         course = Course.find(params[:courseId].to_i)
-        section = course.sections[params[:sectionNum].to_i]
-        lesson = section.lessons.create(lesson_params)
+        if course.creator == current_user
+            section = course.sections[params[:sectionNum].to_i]
+            lesson = section.lessons.create(lesson_params)
 
-        video_ids = params[:video_ids]
-        videos = video_ids.map do |id|
-            ytvideo = Yt::Video.new id: id
-            video = lesson.videos.create(url: "https://www.youtube.com/watch?v=#{id}", embed_url: "https://www.youtube.com/embed/#{id}", title: ytvideo.snippet.data["title"], channel: ytvideo.snippet.data["channelTitle"], description: ytvideo.snippet.data["description"], thumbnail: ytvideo.snippet.data["thumbnails"]["medium"]["url"])
+            video_ids = params[:video_ids]
+            videos = video_ids.map do |id|
+                ytvideo = Yt::Video.new id: id
+                video = lesson.videos.create(url: "https://www.youtube.com/watch?v=#{id}", embed_url: "https://www.youtube.com/embed/#{id}", title: ytvideo.snippet.data["title"], channel: ytvideo.snippet.data["channelTitle"], description: ytvideo.snippet.data["description"], thumbnail: ytvideo.snippet.data["thumbnails"]["medium"]["url"])
+            end
+
+            puts videos
+
+            render json: lesson
+        else
+            render_unauthorized_response
         end
-
-        puts videos
-
-        render json: lesson
     end
 
     def update
@@ -43,5 +47,9 @@ class LessonsController < ApplicationController
 
     def find_lesson
         course = Course.find(params[:course_id].to_i).sections[params[:section_id].to_i].lessons[params[:id].to_i]
+    end
+
+    def render_unauthorized_response
+        render json: {error: "You Did not create this course."}, status: :unauthorized
     end
 end
