a83f688313087dc7197067ee30ad19038782208a
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-23 21:46:28 +0200

    add update interests, set user, set other user to userSlice

--- client/src/features/User/userSlice.js
+++ client/src/features/User/userSlice.js
@@ -3,7 +3,8 @@ import {createSlice } from "@reduxjs/toolkit"
 const slice = createSlice({
     name: 'user',
     initialState:{
-        user: null, 
+        user: null,
+        other: null,
     }, 
     reducers:{
         login: (state, action)=>{
@@ -14,12 +15,21 @@ const slice = createSlice({
         }, 
         addCourse: (state, action)=>{
             state.user.courses.push(action.payload)
+        },
+        updateInterests:(state, action)=>{
+            state.user.interests.push(action.payload)
+        },
+        setUser: (state, action)=>{
+            state.user = action.payload
+        }, 
+        setOther: (state, action)=>{
+            state.other = action.payload
         }
     }
 })
 
-const {login, logout, addCourse} = slice.actions
+const {login, logout, addCourse, updateInterests, setUser, setOther} = slice.actions
 
-export {login, logout, addCourse}
+export {login, logout, addCourse, updateInterests, setUser, setOther}
 
 export default slice.reducer
