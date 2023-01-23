96ef418210999c30d5e78e6d0a6d9ac16087bf86
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-23 21:54:25 +0200

    add include courses and lessons to user create render

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         if user&.authenticate(params[:password])
           session[:user_id] = user.id
           puts session[:user_id]
-          render json: user, status: :created
+          render json: user, include: ['courses', 'courses.sections', 'courses.sections.lessons', 'created_courses', 'created_courses.sections', 'created_courses.sections.lessons'], status: :created
         else
           render json: { error: "Invalid username or password" }, status: :unauthorized
         end
