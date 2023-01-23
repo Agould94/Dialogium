f870e07d4aeb894de7f046272400ee6e1dc6a778
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-23 21:53:40 +0200

    include sections in created course serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :email, :interests, :courses, :created_courses, :full_name
 
+  has_many :created_courses, include: ['sections']
   has_many :courses
 end
