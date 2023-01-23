4234df58fc29d58d2cbb80a4d18dd3d8251e9e73
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-23 21:52:33 +0200

    update course list to filter courses

--- client/src/features/Courses/CourseList.js
+++ client/src/features/Courses/CourseList.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState } from 'react'
 
 import {useSelector, useDispatch} from "react-redux"
 
-import { setCourses } from './courseSlice'
+import { setCourses, filterCourses } from './courseSlice'
 
-import { Box, Card, CardContent, Button, Typography, Grid } from '@mui/material'
+import { Box, Card, CardContent, Button, Typography, Grid, TextField } from '@mui/material'
 
 import CourseCard from './CourseCard'
 
 function CourseList() {
     const dispatch = useDispatch()
     const [loading, setLoading] = useState(true)
+    const [search, setSearch] = useState("")
     const courses = useSelector(state=> state.courses.courses)
+    const filteredCourses = useSelector(state=>state.courses.filteredCourses)
 
+    function handleFilterCourses(e){
+        dispatch(filterCourses(e.target.value))
+    }
     
     console.log(courses)
     useEffect(()=>{
@@ -27,13 +32,24 @@ function CourseList() {
     let coursesToDisplay
   
      if(loading === false){
+        {Object.keys(filteredCourses).length > 0 ? 
+            coursesToDisplay = filteredCourses.map((course)=>{
+                return(
+                    <Box sx={{justifyContent: 'center'}}>
+                        <CourseCard course = {course}></CourseCard>
+                    </Box>
+                )
+                })
+            :
         coursesToDisplay = courses.map((course)=>{
         return(
             <Box sx={{justifyContent: 'center'}}>
                 <CourseCard course = {course}></CourseCard>
             </Box>
         )
-    })
+        })
+        }
+
     }
 
     return (
@@ -43,6 +59,9 @@ function CourseList() {
             <div>loading...</div>
             :
             <div>
+            <Box sx = {{display: "flex", justifyContent: "flex-end"}}>
+                <TextField label = "Search..." variant = "standard" onChange = {handleFilterCourses}></TextField>
+            </Box>
             <Box sx = {{display: 'grid', gridTemplateColumns: 'repeat(4, 2fr)', gap: 2, m: 3, flexWrap: 'wrap' }} container spacing={3}>
                 {coursesToDisplay}
             </Box>
