e10901a56f7f10f54d98e10dbdd66d7a704bcb81
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-25 22:03:17 +0200

    fix reroute on login

--- client/src/features/User/Login.js
+++ client/src/features/User/Login.js
@@ -2,11 +2,15 @@ import React, {useState, useEffect} from 'react'
 import { useDispatch } from 'react-redux'
 import { login } from './userSlice'
 
+import { useHistory } from 'react-router-dom'
+
+import {Box, Typography, TextField, Button, Card, CardContent} from "@mui/material"
+
 function Login({ setUser }) {
   const [firstName, setFirstName] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch()
-  //const history = useHistory()
+  const history = useHistory()
   function handleSubmit(e) {
     e.preventDefault();
     fetch("/login", {
@@ -19,35 +23,41 @@ function Login({ setUser }) {
       if (r.ok) {
         r.json().then((user) => {
           dispatch(login(user))
-        //history.push('/')
+          history.push('/')
       });
       }
     });
   }
 
   return (
-    <div>
+    <Box sx = {{display: "flex", justifyContent: "center", alignContent:"center"}}>
+      <Card sx = {{marginTop: 10, width: 320}}>
       <form onSubmit={handleSubmit}>
-        <h1>Login</h1>
-        <label htmlFor="firstName">First Name</label>
-        <input
+        <CardContent sx={{display: "flex", flexDirection: "column", justifyContent: "center", }}>
+        <Typography variant = "h4" sx = {{display: "flex", marginBottom: 3, justifyContent: "center", marginLeft:-1}}>Login</Typography>
+        <TextField
+          label = "First Name"
           type="text"
           id="loginFirstName"
           autoComplete="off"
           value={firstName}
           onChange={(e) => setFirstName(e.target.value)}
+          sx={{marginBottom: 4}}
         />
-        <label htmlFor="password">Password</label>
-        <input
+        <TextField
+          label = "password"
           type="password"
           id="loginPassword"
           autoComplete="current-password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          sx={{marginBottom: 4}}
         />
-        <button type="submit">Login</button>
+        <Button variant= "contained" type="submit">Login</Button>
+        </CardContent>
       </form>
-    </div>
+      </Card>
+    </Box>
   );
 }
 
