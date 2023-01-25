6a6c9ad8027f735baad87be303a4bcb775940181
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-25 15:02:02 +0200

    Clean Up Code

--- client/src/features/Courses/CourseCard.js
+++ client/src/features/Courses/CourseCard.js
@@ -27,7 +27,6 @@ function CourseCard({course}) {
     }
 
     const allLessons = course.sections.map((section)=> section.lessons).flat()
-    //console.log(allLessons)
     let firstVideoLesson
     let image = Default
     if (allLessons.some(lesson => lesson.videos.length >0)){
@@ -37,9 +36,6 @@ function CourseCard({course}) {
         }
     }
     
-   
-
-    //const image = course.sections[0].lessons[0].thumbnail || Default
     
   return (
     <Card onClick = {handleCourseClick} sx = {{width: 320}}>
