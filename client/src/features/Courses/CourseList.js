4fec7ae1c817dd7da64ece04a46be530d26a60bc
Agould94 <77901585+Agould94@users.noreply.github.com>
2023-01-25 15:01:38 +0200

    Add filter by category functionality to course list

--- client/src/features/Courses/CourseList.js
+++ client/src/features/Courses/CourseList.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 
 import {useSelector, useDispatch} from "react-redux"
 
-import { setCourses, filterCourses } from './courseSlice'
+import { setCourses, filterCourses, setCategories, filterCoursesByCategory } from './courseSlice'
 
-import { Box, Card, CardContent, Button, Typography, Grid, TextField } from '@mui/material'
+import { Box, Card, CardContent, Button, Typography, Grid, TextField, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 
 import CourseCard from './CourseCard'
 
@@ -12,36 +12,43 @@ function CourseList() {
     const dispatch = useDispatch()
     const [loading, setLoading] = useState(true)
     const [search, setSearch] = useState("")
+    const [category, setCategory] = useState(null)
     const courses = useSelector(state=> state.courses.courses)
     const filteredCourses = useSelector(state=>state.courses.filteredCourses)
-
+    const coursesByCategory = useSelector(state =>state.courses.coursesByCategory)
+    const categories = useSelector(state=>state.courses.categories)
     function handleFilterCourses(e){
         dispatch(filterCourses(e.target.value))
     }
-    
-    console.log(courses)
+
     useEffect(()=>{
         fetch('/courses')
         .then((r)=>r.json())
         .then((courses)=>{
             dispatch(setCourses(courses))
-            setLoading(false)
+            fetch("/all_categories")
+            .then((r)=>r.json())
+            .then((categories)=>{
+                dispatch(setCategories(categories))
+                setLoading(false)
+            })
+            
         })
     }, [])
 
     let coursesToDisplay
   
      if(loading === false){
-        {Object.keys(filteredCourses).length > 0 ? 
-            coursesToDisplay = filteredCourses.map((course)=>{
+        if(filteredCourses){ 
+            coursesToDisplay = (category ? filteredCourses.filter((course)=>course.category===category) : filteredCourses).map((course)=>{
                 return(
                     <Box sx={{justifyContent: 'center'}}>
                         <CourseCard course = {course}></CourseCard>
                     </Box>
                 )
                 })
-            :
-        coursesToDisplay = courses.map((course)=>{
+        }else{
+        coursesToDisplay = (category ? courses.filter((course)=> course.category===category) : courses).map((course)=>{
         return(
             <Box sx={{justifyContent: 'center'}}>
                 <CourseCard course = {course}></CourseCard>
@@ -52,6 +59,18 @@ function CourseList() {
 
     }
 
+    function handleCategoryChange(e){
+            setCategory(e.target.value)
+        }
+    let categoriesToDisplay
+    if(loading == false){
+        categoriesToDisplay = categories.map((category)=>{
+            return(
+                <MenuItem key = {category} value = {category}>{category}</MenuItem>
+            )
+        })
+    }
+
     return (
         
         <div>
@@ -60,6 +79,13 @@ function CourseList() {
             :
             <div>
             <Box sx = {{display: "flex", justifyContent: "flex-end"}}>
+                <FormControl variant = "standard" sx = {{width: "200px", marginRight: 2}}>
+                    <InputLabel>Category</InputLabel>
+                    <Select value = {category} label = "Category" onChange = {handleCategoryChange}>
+                        <MenuItem value = {false}>All</MenuItem>
+                        {categoriesToDisplay}
+                    </Select>
+                </FormControl>
                 <TextField label = "Search..." variant = "standard" onChange = {handleFilterCourses}></TextField>
             </Box>
             <Box sx = {{display: 'grid', gridTemplateColumns: 'repeat(4, 2fr)', gap: 2, m: 3, flexWrap: 'wrap' }} container spacing={3}>
